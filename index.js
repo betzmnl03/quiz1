573a3b8fcf805932d65ae0dadcbe7c0266a67773
betsy_manuel <betsy.manuel03@gmail.com>
2020-12-21 19:06:43 -0800

    final

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const cluckRouter = require('./routes/cluck')
 app.use('/',cluckRouter);
 
 const ADDRESS = 'localhost';
-const PORT = 9009;
+const PORT = 3000;
 app.listen(PORT, ADDRESS, () => {
   console.log(`Server listening on ${ADDRESS}:${PORT}`);
 });
